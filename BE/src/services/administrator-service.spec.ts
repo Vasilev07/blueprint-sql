2004a6f127b10c359a0a83aaa36ce9294f6644f4
georgevasilev <georgevasilev07@gmail.com>
2024-02-21 16:48:48 +0200

    add integration test implementation

--- BE/src/services/administrator-service.spec.ts
+++ BE/src/services/administrator-service.spec.ts
@@ -1,4 +1,10 @@
+import { TestDataSource } from '../data-source';
+
 describe('AdministratorService', () => {
+    beforeEach(async () => {
+        await TestDataSource.initialize();
+    });
+
     it('should be defined', () => {
         expect(1).toBe(1);
     });
