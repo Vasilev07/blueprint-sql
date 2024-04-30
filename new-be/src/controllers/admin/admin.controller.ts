7e259c2e01b7a4e969177ab684cd0a6ccc46fcf7
georgevasilev <georgevasilev07@gmail.com>
2024-04-30 09:17:51 +0300

    refactor

--- new-be/src/controllers/admin/admin.controller.ts
+++ new-be/src/controllers/admin/admin.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { AdministratorDTO } from "src/models/administrator-dto";
+import { AdministratorService } from "src/services/administrator.service";
 
 @Controller("/auth")
 export class AdminController {
+    constructor(private administratorService: AdministratorService) {}
+
     @Get("/all")
     async getAll(): Promise<any> {
         return "0;0";
+    }
+
+    @Post("/register")
+    async register(@Body() administratorDTO: AdministratorDTO): Promise<any> {
+        try {
+            return await this.administratorService.register(administratorDTO);
+        } catch (error) {
+            console.error("Error registering admin.", error);
+        }
     }
 }
