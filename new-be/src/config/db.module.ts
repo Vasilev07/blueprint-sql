96c93dfbf9fd8f68c84ba8bc413bdf81eae01bd2
georgevasilev <georgevasilev07@gmail.com>
2024-05-24 15:21:36 +0300

    wip

---
+++ new-be/src/config/db.module.ts
@@ -0,0 +1,40 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Administrator } from "src/entities/administrator.entity";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                const isTesting = configService.get("NODE_ENV") === "test";
+                console.log("isTesting", isTesting);
+                console.log("NODE_ENV", configService.get("NODE_ENV"));
+                console.log("after tests start", configService);
+
+                return {
+                    type: "postgres",
+                    host: "0.0.0.0",
+                    port: 5432,
+                    username: "postgres",
+                    password: "postgres",
+                    database: isTesting
+                        ? "blueprint-sql"
+                        : "blueprint-sql-test",
+                    synchronize: true,
+                    logging: true,
+                    entities: [Administrator],
+                    migrations: [],
+                    subscribers: [],
+                };
+            },
+        }),
+    ],
+    controllers: [],
+    providers: [],
+})
+export class DbModule {}
--- new-be/test/controllers/admin.controller.e2e-spec.ts
+++ new-be/test/controllers/admin.controller.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "src/app.module";
 import { createTestDataSource } from "src/config/db.config";
 import { AdministratorService } from "src/services/administrator.service";
 import { DataSource } from "typeorm";
-import { AppModule } from "src/app.module";
 import { TestDBInitiator } from "../config.e2e";
+import { DbModule } from "src/config/db.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigService } from "@nestjs/config";
 import { Administrator } from "src/entities/administrator.entity";
 
 describe("AdminController (e2e)", () => {
@@ -17,40 +17,15 @@ describe("AdminController (e2e)", () => {
 
     beforeAll(async () => {
         databaseConfig = new TestDBInitiator();
+        console.log("db options in test", databaseConfig.dbOptions);
+
         dataSource = await createTestDataSource(databaseConfig.dbOptions);
 
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [
-                TypeOrmModule.forRootAsync({
-                    inject: [ConfigService],
-                    useFactory: (configService: ConfigService) => {
-                        const isTesting =
-                            configService.get("NODE_ENV") === "test";
-                        console.log("isTesting", isTesting);
-                        console.log("NODE_ENV", configService.get("NODE_ENV"));
-                        console.log(
-                            "after tests start",
-                            configService.get("DATABASE_URL"),
-                        );
-
-                        return {
-                            type: "postgres",
-                            host: "0.0.0.0",
-                            port: 5432,
-                            username: "postgres",
-                            password: "postgres",
-                            database: isTesting
-                                ? "blueprint-sql"
-                                : "blueprint-sql-test",
-                            synchronize: true,
-                            logging: false,
-                            entities: [Administrator],
-                            migrations: [],
-                            subscribers: [],
-                        };
-                    },
-                }),
                 AppModule,
+                DbModule,
+                TypeOrmModule.forFeature([Administrator]),
             ],
         }).compile();
 
@@ -67,10 +42,10 @@ describe("AdminController (e2e)", () => {
         await app.close();
     }, 10000);
 
-    it("/ (GET)", async () => {
+    it.only("register", async () => {
         const administratorDTO = {
             fullName: "Georgi Vasilev",
-            email: "georgevasilev10117@gmail.com",
+            email: "georgevasile11v07@gmail.com",
             password: "123456",
             confirmPassword: "123456",
         };
