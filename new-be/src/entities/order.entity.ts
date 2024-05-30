136fa9b357c1fd681eb6db787e2efc7252d93794
georgevasilev <georgevasilev07@gmail.com>
2024-05-31 01:55:11 +0300

    add order service

--- new-be/src/entities/order.entity.ts
+++ new-be/src/entities/order.entity.ts
@@ -4,7 +4,7 @@ import { Product } from "./product.entity";
 export enum OrderStatus {
     PENDING = "pending",
     COMPLETED = "completed",
-    CALCELLED = "cancelled",
+    CANCELED = "canceled",
 }
 
 @Entity()
---
+++ new-be/src/services/order.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { EntityManager } from "typeorm";
+
+@Injectable()
+export class OrderService {
+    constructor(private entityManager: EntityManager) {}
+
+    async createOrder() {
+        this.entityManager.save();
+        // Create order
+    }
+
+    async getOrder() {
+        // Get order
+    }
+
+    async updateOrder() {
+        // Update order
+    }
+
+    async deleteOrder() {
+        // Delete order
+    }
+}
