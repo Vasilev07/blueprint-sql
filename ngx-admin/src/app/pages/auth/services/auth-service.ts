b63e2d9bab876104ee6cfcb9a8a17ec839a87dab
gdimov <g.dimov1232@gmail.com>
2024-06-01 00:33:37 +0300

    |gd| Reloading a page no longer logs you out

--- ngx-admin/src/app/pages/auth/services/auth-service.ts
+++ ngx-admin/src/app/pages/auth/services/auth-service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { NbAuthJWTToken, NbTokenService } from "@nebular/auth";
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { NbAuthJWTToken, NbTokenService } from '@nebular/auth';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    constructor(private tokenService: NbTokenService, private router: Router) { }
+    constructor(private tokenService: NbTokenService, private router: Router) {
+        this.tokenService.tokenChange().subscribe((token: NbAuthJWTToken) => {
+            token.isValid() ?
+                this.isAuthenticated = true :
+                this.isAuthenticated = false;
+        });
+    }
 
     isAuthenticated: boolean = false;
 
