081e94f87f78cd5847785f643b6f647c76b020a7
georgevasilev <georgevasilev07@gmail.com>
2024-06-02 18:46:19 +0300

    wip

--- new-be/src/controllers/order/order.controller.ts
+++ new-be/src/controllers/order/order.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from "@nestjs/common";
+import {Body, Controller, Post} from "@nestjs/common";
+import { OrderDTO } from "../../models/order-dto";
+import { OrderService } from "../../services/order.service";
 
 @Controller("/order")
-export class OrderController {}
+export class OrderController {
+    constructor(private readonly orderService: OrderService) {}
+
+    @Post("/create")
+    async createOrder(@Body() orderDTO: OrderDTO): Promise<OrderDTO> {
+        try {
+            return await this.orderService.createOrder(orderDTO);
+        } catch (error) {
+            throw new Error("Error creating order" + error);
+        }
+    }
+}
