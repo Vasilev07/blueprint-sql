6b8ea040a2d1e3f24dd74f4c4e244485e0fbe9f3
georgevasilev <georgevasilev07@gmail.com>
2024-06-08 02:10:41 +0300

    Fix Mappers

--- new-be/src/models/product-dto.ts
+++ new-be/src/models/product-dto.ts
@@ -1,19 +1,25 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { OrderDTO } from "./order-dto";
+import {AutoMap} from "@automapper/classes";
 
 export class ProductDTO {
+    @AutoMap()
     @ApiProperty()
     id?: number;
 
+    @AutoMap()
     @ApiProperty()
     name: string;
 
+    @AutoMap()
     @ApiProperty()
     weight: number;
 
+    @AutoMap(() => OrderDTO)
     @ApiProperty()
     order?: OrderDTO;
 
+    @AutoMap()
     @ApiProperty()
     price: number;
 }
