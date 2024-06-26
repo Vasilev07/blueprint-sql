9589f42e86f478833149fdc0f5af2ea3e5371b1a
georgevasilev <georgevasilev07@gmail.com>
2024-06-26 22:14:07 +0300

    Refactor

--- new-fe/src/app/services/auth.service.ts
+++ new-fe/src/app/services/auth.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import moment from "moment";
 import { JwtHelperService } from "@auth0/angular-jwt";
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class AuthService {
     constructor(
         private readonly http: HttpClient,
